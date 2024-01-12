a6feaed0f5c70413cc75e1166a5631ab45434d1f
Gi-bo <bonenfant.gilles@gmail.com>
2024-01-12 08:51:20 +0100

    affichage single et home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_single')]
+    public function single(Article $article): Response
+    {
+        return $this->render('home/single.html.twig', [
+            'article' => $article,
         ]);
     }
 }
